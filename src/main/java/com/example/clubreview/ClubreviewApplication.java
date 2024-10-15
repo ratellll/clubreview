bf1f60dc6a77219c7f4a936fbe292d5f2e1686b6
ratellll <latellbin@naver.com>
2024-10-15 15:44:48 +0900

    feat: resume start

---
+++ src/main/java/com/example/clubreview/ClubreviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.clubreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clubreview/ClubreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clubreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
