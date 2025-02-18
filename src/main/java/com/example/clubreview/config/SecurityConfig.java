e60df069553a0732dd5f0bf287fbee44cefb3e9c
ratellll <latellbin@naver.com>
2025-02-18 18:19:39 +0900

    feat: config수정

--- src/main/java/com/example/clubreview/config/SecurityConfig.java
+++ src/main/java/com/example/clubreview/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/","/error","/users/register/**","/users/**", "/h2-console/**", "/uploads/**").permitAll() // 공개 경로
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/static/**").permitAll() // 정적 리소스 접근 허용
                         .requestMatchers("/clubs/list", "/clubs/{id}", "/clubs/details/**", "reviews/user/**","/mypage/**").authenticated() // 클럽 리스트와 상세 페이지는 로그인 필요
                         .requestMatchers("/clubs/admin/**", "/reviews/admin/**","admin/users/**").hasRole("ADMIN") // ADMIN 권한 필요
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
