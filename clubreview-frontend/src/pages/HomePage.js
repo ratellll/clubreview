2e7b1187abea971f65fa9b0878aa8673486cd420
ratellll <latellbin@naver.com>
2025-07-25 16:24:50 +0900

    FEAT: 리팩토링 시작

---
+++ clubreview-frontend/src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ApiTest from '../components/common/ApiTest';
+
+const HomePage = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-body text-center">
+                            <h1 className="card-title">🎉 클럽의 민족</h1>
+                            <p className="card-text">React 프론트엔드가 성공적으로 실행되었습니다!</p>
+                        </div>
+                    </div>
+                    <ApiTest />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
